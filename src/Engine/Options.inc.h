2c0237e25222947710bd411a125b9cc1a858a995
Finn Bacall <finn.bacall@manchester.ac.uk>
2019-03-26 16:22:48 +0000

    Add hotkeys for top-level basescape menu.

--- src/Engine/Options.inc.h
+++ src/Engine/Options.inc.h
@@ -56,6 +56,11 @@ OPT SDLKey keyGeoUfoTracker, keyGeoTechTreeViewer, keyGeoGlobalResearch, keyGeoG
 	keyBattleActionItem1, keyBattleActionItem2, keyBattleActionItem3, keyBattleActionItem4, keyBattleActionItem5,
 	keyNightVisionToggle, keyNightVisionHold, keySelectMusicTrack;
 
+// Basescape keys
+OPT SDLKey keyBasescapeBuildNewBase, keyBasescapeBaseInfo, keyBasescapeSoldiers, keyBasescapeCrafts,
+	keyBasescapeFacilities, keyBasescapeResearch, keyBasescapeManufacture, keyBasescapeTransfer,
+	keyBasescapePurchase, keyBasescapeSell;
+
 // Extra options (OXCE)
 OPT int oxceStartUpTextMode;
 OPT bool oxceUpdateCheck;
